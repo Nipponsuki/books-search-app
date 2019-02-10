6614a47a7325be9d60e92e73ed71b056d783a6c9
Alex <alex.alexeev23061991@gmail.com>
2019-02-10 05:27:30 +0300

    Updates

---
+++ precache-manifest.33cc24bfaa943c37f4beafd6c6120f4c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "34921081f14633a3906a4b851498d787",
+    "url": "/Nipponsuki/books-search-app/static/media/loading.34921081.gif"
+  },
+  {
+    "revision": "a8fb04f7f1684978da0ed58bbc6ef2f2",
+    "url": "/Nipponsuki/books-search-app/static/media/h-5.a8fb04f7.jpg"
+  },
+  {
+    "revision": "cec3d7419348c3c3959e220bf2524c72",
+    "url": "/Nipponsuki/books-search-app/static/media/book-logo.cec3d741.png"
+  },
+  {
+    "revision": "d7daee2f222465e78183",
+    "url": "/Nipponsuki/books-search-app/static/js/runtime~main.d7daee2f.js"
+  },
+  {
+    "revision": "2f5e6fba44cc99da608f",
+    "url": "/Nipponsuki/books-search-app/static/js/main.2f5e6fba.chunk.js"
+  },
+  {
+    "revision": "458d335eef366333c982",
+    "url": "/Nipponsuki/books-search-app/static/js/1.458d335e.chunk.js"
+  },
+  {
+    "revision": "2f5e6fba44cc99da608f",
+    "url": "/Nipponsuki/books-search-app/static/css/main.23b88249.chunk.css"
+  },
+  {
+    "revision": "5559971ba5125a06a9d415ffeae6136f",
+    "url": "/Nipponsuki/books-search-app/index.html"
+  }
+];
