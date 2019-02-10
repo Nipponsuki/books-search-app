e247a80523311c0a536a6297616283c01adb234d
Alex <alex.alexeev23061991@gmail.com>
2019-02-10 05:24:59 +0300

    first commit

---
+++ build/precache-manifest.959f135699f961cf1a6ea250980d25d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "34921081f14633a3906a4b851498d787",
+    "url": "/static/media/loading.34921081.gif"
+  },
+  {
+    "revision": "a8fb04f7f1684978da0ed58bbc6ef2f2",
+    "url": "/static/media/h-5.a8fb04f7.jpg"
+  },
+  {
+    "revision": "cec3d7419348c3c3959e220bf2524c72",
+    "url": "/static/media/book-logo.cec3d741.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "892496c44e99e6d10128",
+    "url": "/static/js/main.892496c4.chunk.js"
+  },
+  {
+    "revision": "0ecbeefce43550a17c76",
+    "url": "/static/js/1.0ecbeefc.chunk.js"
+  },
+  {
+    "revision": "892496c44e99e6d10128",
+    "url": "/static/css/main.23b88249.chunk.css"
+  },
+  {
+    "revision": "06ac91069bc50aab7a4fd133b7e2dabb",
+    "url": "/index.html"
+  }
+];
